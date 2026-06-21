bbfa86d2e3a6f1568727e70698c4cf6e4e674182
loucifabdelmouaiz-sketch <loucifabdelmouaiz@gmail.com>
2026-06-21 15:38:49 +0200

    Add files via upload

---
+++ import streamlit as st.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="SMART FNS", layout="wide")
+st.title("👩‍⚕️ SMART FNS - Plateforme Médicale")
+
+st.info("المنصة عادت للعمل! الآن يمكنكِ إضافة حالاتكِ التعليمية هنا.")
+st.write("هذه هي النسخة الجديدة، جاهزة للتعديل.")
